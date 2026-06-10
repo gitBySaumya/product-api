148b7d22704b488a43ac93f815547be15db231f9
Saumya Sharma <saumya.sharma11082006@gmail.com>
2026-06-10 17:45:14 +0530

    Added CORS

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
+// middleware
+app.use(cors());
 app.use(express.json());
 
 // routes
@@ -13,7 +17,8 @@ app.use('/api/products', productRoutes);
 // enquiry routes
 app.use('/api', enquiryRoutes);
 
-const PORT = 3000;
+// port
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
